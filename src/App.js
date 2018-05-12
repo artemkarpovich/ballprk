82cfb01612cc26975b166679635fb7e94cd7ef0e
Artem <karpartem94@gmail.com>
2018-05-12 16:30:52 +0300

    Add react router support

--- src/App.js
+++ src/App.js
@@ -1,11 +1,33 @@
 import React, { Component } from 'react';
+import {
+  BrowserRouter as Router,
+  Route,
+  Link,
+} from 'react-router-dom';
+import { Layout1Container } from './modules/layout-1';
+import { Layout2Container } from './modules/layout-2';
 import './App.css';
+
+const Home = () => (
+  <div className="home">
+    <ul>
+      <li><Link to="/layout-1">About</Link></li>
+      <li><Link to="/layout-2">Topics</Link></li>
+    </ul>
+  </div>
+);
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        #BALLPRK
+      <div className="app">
+        <Router>
+          <div className="main-wrapper">
+            <Route exact path="/" component={Home} />
+            <Route path="/layout-1" component={Layout1Container}/>
+            <Route path="/layout-2" component={Layout2Container}/>
+          </div>
+        </Router>
       </div>
     );
   }
--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
+import { BrowserRouter } from 'react-router-dom'
 import './index.css';
 import App from './App';
 import createStore from './createStore';
@@ -10,6 +11,8 @@ const store = createStore();
 
 ReactDOM.render(
   <Provider store={store}>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </Provider>, document.getElementById('root'));
 registerServiceWorker();
